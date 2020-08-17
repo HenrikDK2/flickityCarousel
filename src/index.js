3d416be1d39204787f2f0f05c34a1e52406801e3
Henrik Mundt Milo <4519362@RTS-365.dk>
2020-08-17 12:24:18 +0200

    Initial

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  @keyframes slideTopIn {
+    0%{
+      transform:translateY(-300px);
+    }
+
+    100%{
+      transform:translateY(0);
+    }
+  }
+
+  @keyframes slideTopOut {
+    0%{
+      transform:translateY(0);
+    }
+
+    100%{
+      transform:translateY(-300px);
+    }
+  }
+
+  @keyframes slideBottomIn {
+    0%{
+      transform:translateY(300px);
+    }
+
+    100%{
+      transform:translateY(0);
+    }
+  }
+
+  @keyframes slideBottomOut {
+    0%{
+      transform:translateY(0);
+    }
+
+    100%{
+      transform:translateY(300px);
+    }
+  }
+
+
+  :root{
+    --bgColor: #030203;
+    --red: #ff2a70;
+  }
+`;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
